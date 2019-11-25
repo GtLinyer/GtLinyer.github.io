7fac802e333a413cf8902b44c3ab8af6973d000d
Linyer <42068542+GtLinyer@users.noreply.github.com>
2019-11-26 01:14:09 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,3 +13,26 @@ function topnavChange()
 		node1.classList.add('shownav');
 	}	
 }
+function date() {
+	var d = new Date();
+	var wk = new Array('星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日');
+	var yy = d.getFullYear();
+	var MM = d.getMonth()+1;
+	var ww = d.getDay()-1;
+	var dd = d.getDate();
+	var hhh = d.getHours();
+	var mmm = d.getMinutes();
+	var sss = d.getSeconds();
+	var hh = checkTime(hhh);
+	var mm = checkTime(mmm);
+	var ss = checkTime(sss);
+	function checkTime(i){
+		if(i<10){
+			i= "0" + i;
+		}
+		return i;
+	}
+	var nowdate = "当前时间为："+yy+"年"+MM+"月"+dd+"日 "+wk[ww]+" "+hh+"时"+mm+"分"+ss+"秒";
+	return nowdate;
+}
+setInterval("document.getElementById('date').innerHTML = date();",500);
